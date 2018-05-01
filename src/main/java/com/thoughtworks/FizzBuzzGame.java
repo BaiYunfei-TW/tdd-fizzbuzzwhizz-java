0819d948df6091e026e2a0082e2fcfdecea82870
BaiYunfei-TW <baiyunfei_tw@foxmail.com>
2018-05-01 21:30:06 +0800

    实现

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -8,11 +8,34 @@ public class FizzBuzzGame {
     private List<String> results = new ArrayList<>();
 
     public void start(int count) {
-
+        for (int i = 1; i <= count; i++) {
+            results.add(parseNumberToWords(i));
+        }
     }
 
     public List<String> getResults() {
         return results;
+    }
+
+    public String parseNumberToWords(int num) {
+        if ((num+"").indexOf('3') != -1) {
+            return "Fizz";
+        }
+        String result = "";
+        if (num % 3 == 0) {
+            result += "Fizz";
+        }
+        if (num % 5 == 0) {
+            result += "Buzz";
+        }
+        if (num % 7 == 0) {
+            result += "Whizz";
+        }
+        if ("".equals(result)) {
+            return num + "";
+        } else {
+            return result;
+        }
     }
 }
 
